b8bf9314cadfd62fe01e686f621cbd822f4a9d83
Cristian Racedo <CristianDRacedo@hotmail.com>
2023-03-11 11:28:29 -0300

    crear componente tabla dinamica

---
+++ src/components/table.js
@@ -0,0 +1,37 @@
+const Table = (props) => props.data == '' ? 
+'No se consultaron Usuarios' :
+<table className="table table-striped table-dark">
+    <caption>usuarios</caption>
+    <TableHead row={props.data[0]}/>
+    <TableBody rows={props.data} />
+</table>
+
+const TableHead = (props) => {
+const titles = getHeadings(props.row);
+return (
+<thead>
+    <tr>
+        { titles.map((th,i) => <th key={"th-" + i}>{th}</th>) }
+    </tr>
+</thead>
+) }
+
+const TableBody = (props) => {
+    const data = props.rows;
+    const titles = getHeadings(props.rows[0]);
+    return(
+        <tbody>
+        { data.map((row,i) => <tr key={"row-" + i}>
+            { titles.map((t,i) => typeof row[t] == 'object' ? 
+                <td key={"td-" + i}>para mapear</td> : 
+                <td key={"td-" + i}>{ row[t] }</td>
+            ) }
+        </tr> ) }
+        </tbody>
+    )
+}
+
+function getHeadings(obj){
+    return Object.keys(obj);
+}
+export default Table;
