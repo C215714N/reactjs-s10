c6c3739d7867a007289e395426c9caccf29a0c94
Cristian Racedo <CristianDRacedo@hotmail.com>
2023-03-11 11:28:12 -0300

    consultar servidor con fetchAPI

--- src/components/nav.js
+++ src/components/nav.js
@@ -1,22 +1,24 @@
 import { NavLink } from "react-router-dom";
 export default function Nav(props){
-    const { url, appName, links, action, users, setUsers } = props;
+    const { url, appName, links, action } = props;
     return(
-        <nav>
-            <a href="#">{ appName }</a>
-            <button onClick={async() => {
-                action(url +"users?id=1"); 
-                await console.log(users)
-                }}>
-                consultar usuarios
-            </button>
-            <List links={ links } />
+        <nav className="navbar navbar-dark navbar-expand-md | bg-dark bg-gradient | px-3 sticky-top">
+            <a href="#" className="navbar-brand">{ appName }</a>
+            <button className="navbar-toggler navbar-toggler-icon"></button>
+            <List links={ links } action={action} url={url} />
         </nav>
 ) }
 
-const List = (props) => <ul> { props.links.map( (link, i) => 
+const List = (props) => {
+const { url, action, links} = props;
+return(
+<ul className="navbar-nav navbar-collapse"> { links.map( (link, i) => 
     <li key={i} className="nav-item">
-        <NavLink to={link.href}> <span className={ link.className }></span> {link.text}</NavLink>
-    </li>
+        <NavLink to={link.href} onClick={() => action(url + link.href)} className="nav-link">
+            <span className={ "pe-2 | " + link.className }></span>
+            {link.text}
+        </NavLink>
+    </li> 
     ) } 
-</ul>
+</ul> 
+) }
