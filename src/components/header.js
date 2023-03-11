aee6e2d2420d5fc43a8c4177cd1b961da06164f8
Cristian Racedo <CristianDRacedo@hotmail.com>
2023-03-11 14:45:44 -0300

    crear componente busqueda en cabecera

--- src/components/header.js
+++ src/components/header.js
@@ -1,5 +1,17 @@
-export default function Header(){
+function Header(props){
+    const { action, appName } = props
+    const key = "LanYkWCgNLIRDm6XZOZWnYH9yZHOProA",
+    limit = 20,
+    offset = 0,
+    rating = "g",
+    lang = "es";
+
     return(
-        <p>Soy el Header</p>
+        <header className="vh-100 | d-grid justify-content-center align-content-center | text-light bg-dark bg-gradient">
+            <h1 className="h1">Bienvenido a {appName}</h1>
+            <input className="form-control" type="search" onInput={ (e) => action(`https://api.giphy.com/v1/gifs/search?api_key=${key}&q=${e.target.value}&limit=${limit}&offset=${offset}&rating=${rating}&lang=${lang}`)}/>
+        </header>
     )
 }
+
+export default Header;
