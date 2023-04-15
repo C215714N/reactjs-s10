27f61d158f68bd5a14a15a656b10f690e73db827
Cristian Racedo <CristianDRacedo@hotmail.com>
2023-04-15 11:44:32 -0300

    importar componente DataList

--- src/components/pagination.jsx
+++ src/components/pagination.jsx
@@ -1,14 +1,23 @@
+import { DataList } from "./DataList";
+
 function Pagination (props){
     const { total_count, count, offset } = props.pagination;
-    
+    const current = offset / count + 1 || 0,
+        total = Math.ceil(total_count / count) || 0;
+    const pageData = {
+        current,
+        total,
+        ...props.pagination
+    }
     return(
         <footer className="text-center">
-            <p className="m-0">{
-                (offset / count + 1) + "/" + 
-                Math.ceil(total_count / count)
-            }
-            </p>
+            <DataList pagination={pageData}/>
             <button className="btn btn-outline-dark"><span className="fa-solid fa-chevron-left"></span></button>
+            { current >= 3 ? <button className="btn btn-outline-dark">{current - 1}</button> : ""}
+            <button className="btn btn-dark">
+                { current }
+            </button>
+            { current ? <button className="btn btn-outline-dark">{current + 1}</button> : "" }
             <button className="btn btn-outline-dark"><span className="fa-solid fa-chevron-right"></span></button>
         </footer>
     )
