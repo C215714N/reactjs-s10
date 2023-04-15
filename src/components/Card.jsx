e98e71f12b3341565f091f96922f2c82f84dfd9c
Cristian Racedo <CristianDRacedo@hotmail.com>
2023-04-15 10:41:16 -0300

    Crear componente tarjeta

---
+++ src/components/Card.jsx
@@ -0,0 +1,15 @@
+export const Card = ({data}) => 
+<article id={data.id} className="card">
+    <header className="card-header">
+        <img src={data.images ? data.images.fixed_height_small.webp : "logo.svg"} alt={data.title} />
+        <h3 className="card-title">{data.title}</h3>
+    </header>
+    <div className="card-body">
+        <p>{ data.username }</p>
+    </div>
+    <footer className="card-footer d-flex gap-1">
+        <button><i className="fa fa-heart"></i></button>
+        <button><i className="fa fa-thumbs-up"></i></button>
+        <button><i className="fa fa-floppy-disk"></i></button>
+    </footer>
+</article>
