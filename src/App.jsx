eeba87db7c8407be4906aec4d021ddbc40e7b1cf
Cristian Racedo <CristianDRacedo@hotmail.com>
2023-04-15 13:03:25 -0300

    Simplificacion punto de entrada principal

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,3 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 // Componentes
@@ -7,28 +5,16 @@ import Search from './components/search'
 import Results from './components/results'
 import Pagination from './components/pagination'
 
+// Datos Externos
+import ContextAPI from './actions/Context'
+
 function App() {
-  const [results, setResults] = useState({
-    data: [{}],
-    pagination: {
-      total_count: 0,
-      count: 0,
-      offset: 0
-    }
-  })
-
-  async function getData(url){
-    let data = await fetch(url);
-    data = await data.json();
-    setResults(data);
-  }
-
   return(
-    <>
-      <Search getData={getData}/>
-      <Results data={results.data}/>
-      <Pagination pagination={results.pagination}/>
-    </>
+    <ContextAPI>
+      <Search/>
+      {/* <Results/>
+      <Pagination/> */}
+    </ContextAPI>
   )
 }
 
