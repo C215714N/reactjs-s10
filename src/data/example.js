f1d42aed4b1f5fd3f48535bb55da835322b8db59
Cristian Racedo <CristianDRacedo@hotmail.com>
2023-04-15 11:43:48 -0300

    ejemplo manipulacion array y objetos

---
+++ src/data/example.js
@@ -0,0 +1,25 @@
+const array = [10, 13, 25, 23, 14]
+const object = {a: 10, b: 15, c: 30}
+
+array[2] // 25
+object.b // 15
+object["b"] // 15
+
+const datos = [{
+usuario: "c215714n",
+capacitaciones: [
+    "html",
+    "css",
+    "js"
+],
+cursos: [{
+    nombre: "react-js"
+    dias: "sabados",
+    inicio: "10hs",
+    fin: "13hs"
+}]
+}]
+
+datos[0]['cursos'][0][nombre] // react-js
+datos.usuario // c215714n
+Object.keys(datos[0]) // [usuarios, capacitaciones, cursos]
