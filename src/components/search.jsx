1a56914c8e7d3f4c9070693071ace3177a06fa13
Cristian Racedo <CristianDRacedo@hotmail.com>
2023-04-15 13:03:48 -0300

    implementacion de contexto handleState

--- src/components/search.jsx
+++ src/components/search.jsx
@@ -1,17 +1,6 @@
-function Search(props){
-    const {getData } = props;
-
-    function getWord(word){
-        const 
-            API_KEY = "LanYkWCgNLIRDm6XZOZWnYH9yZHOProA",
-            LIMIT = 10,
-            OFFSET = 0,
-            R = "r",
-            LANG = "es",
-            url = `https://api.giphy.com/v1/gifs/search?api_key=${API_KEY}&q=${word}&limit=${LIMIT}&offset=${OFFSET}&rating=${R}&lang=${LANG}`
-        getData(url);
-    }
-
+import { useActionContext } from "../actions/Context";
+function Search(){
+    const handleState = useActionContext();
     return(
         <header className="row p-3">
             <div className="form-floating col p-0">
@@ -19,7 +8,10 @@ function Search(props){
                     id="search" 
                     className="form-control" type="search" 
                     placeholder="Realice una Busqueda"
-                    onInput={(e) => getWord(e.target.value)}/>
+                    onInput={(e) => handleState({
+                        action: "SEARCH",
+                        value: e.target.value
+                    })}/>
                 <label className="px-4" htmlFor="search">Realice una Busqueda</label>
             </div>
             <button className="col-md-4 btn btn-outline-dark | d-flex align-items-center justify-content-center "> <span className="fs-3 fa-solid fa-magnifying-glass"></span>Buscar</button>
