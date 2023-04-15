c9f99fc3236d2134c30cf95c4dddf429652c49b0
Cristian Racedo <CristianDRacedo@hotmail.com>
2023-04-15 10:40:54 -0300

    Mapeo de resultados e insercion de componente Card

--- src/components/results.jsx
+++ src/components/results.jsx
@@ -1,8 +1,15 @@
+import { Card } from './Card';
+
 function Results (props){
     const { data } = props;
     return(
         <main>
-            <h2>{ data ? "Resultados" : "No se realizaron Busquedas"}</h2>
+            <h2>{ data.length > 1 ? 
+                "Resultados" : 
+                "No se realizaron Busquedas"}</h2>
+            <div className="d-flex flex-wrap gap-2">
+            { data.map( (item,i) => <Card key={i} data={item} /> )}
+            </div>
         </main>
     )
 }
